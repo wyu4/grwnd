eed5824bbf34afe69804186057943c84ce7a8499
Wilson <wilsonyu657@gmail.com>
2026-06-02 12:05:12 -0400

    Clear junk

---
+++ src/app/layout.tsx
@@ -0,0 +1,13 @@
+import "./css/globals.css";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className={`h-full antialiased`}>
+      <body className="min-h-full flex flex-col">{children}</body>
+    </html>
+  );
+}
